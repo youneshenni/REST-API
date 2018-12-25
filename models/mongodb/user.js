ac5a45afaff6b0679267bd4eb6579c55b3980b7b
Younes Henni <younabobo3@gmail.com>
2018-12-25 17:54:15 +0100

    Added the conv script to package.json

--- models/mongodb/user.js
+++ models/mongodb/user.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
+  password: String,
 });
 module.exports = mongoose.model('User', UserSchema);
